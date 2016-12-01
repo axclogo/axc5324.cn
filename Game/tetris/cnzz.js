a87d6ad9757299d7ac70eb6435032e627dbaa950
赵新 <axclogo@163.com>
2016-12-01 15:25:49 +0800

    阿萨德

---
+++ Game/tetris/cnzz.js
@@ -0,0 +1,2 @@
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_4787974'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/stat.php%3Fid%3D4787974' type='text/javascript'%3E%3C/script%3E"));
---
+++ Game/tetris/more.js
@@ -0,0 +1,9 @@
+//var playMoreURL = "#";
+//(function() {
+//	var a = document.body;
+//	var b = document.createElement("div");
+//	b.setAttribute("style", "position: fixed;top: 80px; left: 0px; background: rgba(0,0,0,0.8);width: 65px;color: #ffffff;text-align: center;font-size: 12px;text-decoration: none;z-index: 9999999999;");
+//	b.setAttribute("id", "more-downwrap");
+//	b.innerHTML = '<a id="more-btn" href="' + playMoreURL + '" style="color:#fff;text-decoration:none;line-height:20px;">挑战1000分</a>';
+//	a.appendChild(b)
+//})();
