88bff62bed1dd2bf6aadde849b0e49248ed53a93
赵新 <axclogo@163.com>
2017-01-05 15:18:37 +0800

    no message

--- JSON_Data/Axc_Phone_System_JSPath.js
+++ JSON_Data/Axc_Phone_System_JSPath.js
@@ -1,5 +1,5 @@
 defineClass('ViewController', {
     onClick: function() {
-    	    console.log('JS补丁修复1111111111111111111111111111111111111');
+    	    console.log('JS补丁修复王鹏帆哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈');
     },
 });
