3841be0c45c6adccbd0abab54a491e612ffd79e4
赵新 <axclogo@163.com>
2017-02-15 17:48:38 +0800

    sa

--- JSON_Data/Axc_Phone_System_JSPath.js
+++ JSON_Data/Axc_Phone_System_JSPath.js
@@ -2,7 +2,7 @@ require('UIColor');
 defineClass('ViewController', {
     onClick: function() {
 
-        self.testLabel().setText("点击发现补丁修改");
+        self.testLabel().setText("点击发现补丁修改asdasdasdasdasdasdasd");
         self.testLabel().setBackgroundColor(UIColor.clearColor());
     },
 });
